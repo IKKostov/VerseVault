1c45606156442c0e3452f30280b500cabcda033b
IvanKKostov <ivan.k.kostov.2023@elsys-bg.org>
2025-05-21 21:37:42 +0300

    Updated settings.py

--- versevault_project/settings.py
+++ versevault_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_2q86n2-@=(j@6$2ny!l=e6sm&@tpdfmt2*db#9ea9gu&=5lzs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
