b651e0c9970917baacf33937f3888ba42a8b5ec4
IvanKKostov <ivan.k.kostov.2023@elsys-bg.org>
2025-05-30 10:25:49 +0300

    Urls.py change

--- versevault_project/urls.py
+++ versevault_project/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.api_root, name='api-root'), # API root view
+    path('register/', views.RegisterView.as_view(), name='register'), # User registration view
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # Token obtain view 
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Token refresh view
+    path('profile/', views.ProfileView.as_view(), name='profile'),# User profile view 
 ]
 
 if settings.DEBUG:
