4d985b8cbdfd2a7e5ed668bd80ab5bee0cd3dad6
IvanKKostov <ivan.k.kostov.2023@elsys-bg.org>
2025-06-02 23:21:38 +0300

    Added views in views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
-# Create your views here.
+def api_root(request):
+    return Response({"message": "Welcome to VerseVault API!"})
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully!"}, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email,
+        })
