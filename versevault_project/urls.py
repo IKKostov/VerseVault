a1ef019c3c8238cd807bc13e8dbd15bb7bcd0bb0
IvanKKostov <ivan.k.kostov.2023@elsys-bg.org>
2025-06-03 20:12:45 +0300

    Urls.py tiny winy fix

--- versevault_project/urls.py
+++ versevault_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from . import views
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
